6118c1f54f1095522ba9d7627e0e0c8349b5b35e
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2021-08-30 14:39:06 +1000

    Nuke time

--- LibEternal.Unity/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("LibEternal.Unity.Editor")]
--- LibEternal/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-//So we can access internal stuff from our test class
-[assembly: InternalsVisibleTo("LibEternal.Tests")]
-//And our other LibEternal projects
-[assembly: InternalsVisibleTo("LibEternal.Unity")]
-[assembly: InternalsVisibleTo("LibEternal.Unity.Editor")]
