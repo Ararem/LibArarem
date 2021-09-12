7aba248a3d2ed8f81cec7a073481f1ef7848bed7
EternalClickbait <48875125+EternalClickbait@users.noreply.github.com>
2021-09-12 14:24:09 +1000

    Create DelegateDestructurer.cs

---
+++ LibEternal/LibEternal/Logging/Destructurers/DelegateDestructurer.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.Annotations;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace LibEternal.Logging.Destructurers
+{
+	/// <inheritdoc />
+	[PublicAPI]
+	public sealed class DelegateDestructurer : IDestructuringPolicy
+	{
+		/// <inheritdoc />
+		public bool TryDestructure(object value, ILogEventPropertyValueFactory propertyValueFactory, out LogEventPropertyValue? result)
+		{
+			if (value is not Delegate del)
+			{
+				result = null;
+				return false;
+			}
+			if (del is MulticastDelegate multicast)
+			{
+				var delegates = multicast.GetInvocationList();
+				result = new SequenceValue(delegates.Select(m => propertyValueFactory.CreatePropertyValue(m.Method)));
+				return true;
+			}
+			//If it's not multicast we know it only has a single target, so m.MethodInfo will be accurate
+			else
+			{
+				MethodInfo m = del.Method;
+				result = propertyValueFactory.CreatePropertyValue(m);
+				return true;
+			}
+		}
+	}
+}
