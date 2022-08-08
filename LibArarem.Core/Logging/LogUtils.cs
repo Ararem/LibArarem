11f2000fc14a053dbc598dd8d11467b4e606531d
rrm <48875125+EternalClickbait@users.noreply.github.com>
2022-08-09 00:30:03 +1000

    Fix up some logging and try change when UI elements are loaded

--- LibArarem.Core/Logging/LogUtils.cs
+++ LibArarem.Core/Logging/LogUtils.cs
@@ -1,14 +1,33 @@
+using Serilog;
+using Serilog.Context;
 using Serilog.Events;
 using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using static Serilog.Log;
 
 namespace LibArarem.Core.Logging;
 
 /// <summary>Class containing helper methods for logging</summary>
 public static class LogUtils
 {
+	/// <summary>
+	/// Returns a <see cref="ILogger"/> that contains a "Context" property for which object instance is making the log call
+	/// </summary>
+	public static ILogger WithInstanceContext(object obj)
+	{
+		return Log.ForContext("@Context", obj);
+	}
+
+	/// <summary>
+	/// Pushes a property onto the <see cref="LogContext"/> that marks a section of code as extremely verbose (like even more verbose that <see cref="LogEventLevel.Verbose"/>) - e.g. frame update messages
+	/// </summary>
+	/// <seealso cref="LogContext"/>
+	/// <seealso cref="LogContext.PushProperty"/>
+	public static IDisposable MarkContextAsExtremelyVerbose()
+	{
+		return LogContext.PushProperty(nameof(MarkContextAsExtremelyVerbose), true);
+	}
+
 	/// <summary>Logs a message that an event was called, allowing for callback tracing</summary>
 	/// <example>
 	///  <code>
@@ -20,27 +39,45 @@ public static class LogUtils
 	/// </example>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	[StackTraceHidden]
-	public static void TrackEvent(object? sender, EventArgs eventArgs)
+	public static void TrackEvent(this ILogger logger, object? sender, EventArgs eventArgs)
 	{
-		Debug("{CallbackName} from {Sender}: {@EventArgs}", new StackFrame(1).GetMethod(), sender, eventArgs);
+		logger.Debug("{CallbackName} from {@Sender}: {@EventArgs}", new StackFrame(1).GetMethod(), sender, eventArgs);
 	}
 
 	/// <summary>Logs an expression and it's value to the logger, with an optional message</summary>
+	/// <param name="logger">Logger to log expression to</param>
 	/// <param name="value">The value to log</param>
 	/// <param name="message">Optional message to be logged after the expression</param>
 	/// <param name="expressionString">(Please don't set this) string containing the compile time expression passed to <paramref name="value"/></param>
 	/// <typeparam name="T">Type of the expression</typeparam>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	[StackTraceHidden]
-	public static void LogExpression<T>(T value, string? message = null, [CallerArgumentExpression("value")] string expressionString = "Unknown Variable")
+	public static void LogExpression<T>(this ILogger logger, T value, string? message = null, [CallerArgumentExpression("value")] string expressionString = "Unknown Variable")
 	{
 		if (message is null)
-			Verbose("{Expression}: {Value}", expressionString, value);
+			logger.Verbose("{Expression}: {Value}", expressionString, value);
 		else
-			Verbose("{Expression}: {Value}, {Message}", expressionString, value, message);
+			logger.Verbose("{Expression}: {Value}, {Message}", expressionString, value, message);
 	}
 
 	/// <summary>Logs an expression and it's value to the logger, with an optional message</summary>
+	/// <param name="logger">Logger to log expression to</param>
+	/// <param name="value">The value to log</param>
+	/// <param name="message">Optional message to be logged after the expression</param>
+	/// <param name="expressionString">(Please don't set this) string containing the compile time expression passed to <paramref name="value"/></param>
+	/// <typeparam name="T">Type of the expression</typeparam>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	[StackTraceHidden]
+	public static void LogExpressionDestructured<T>(this ILogger logger, T value, string? message = null, [CallerArgumentExpression("value")] string expressionString = "Unknown Variable")
+	{
+		if (message is null)
+			logger.Verbose("{Expression}: {@Value}", expressionString, value);
+		else
+			logger.Verbose("{Expression}: {@Value}, {Message}", expressionString, value, message);
+	}
+
+	/// <summary>Logs an expression and it's value to the logger, with an optional message</summary>
+	/// <param name="logger">Logger to log expression to</param>
 	/// <param name="level"><see cref="LogEventLevel"/> at which to log the event</param>
 	/// <param name="value">The value to log</param>
 	/// <param name="message">Optional message to be logged after the expression</param>
@@ -48,11 +85,11 @@ public static class LogUtils
 	/// <typeparam name="T">Type of the expression</typeparam>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	[StackTraceHidden]
-	public static void LogExpression<T>(LogEventLevel level, T value, string? message = null, [CallerArgumentExpression("value")] string expressionString = "Unknown Variable")
+	public static void LogExpression<T>(this ILogger logger, LogEventLevel level, T value, string? message = null, [CallerArgumentExpression("value")] string expressionString = "Unknown Variable")
 	{
 		if (message is null)
-			Write(level, "{Expression}: {Value}", expressionString, value);
+			logger.Write(level, "{Expression}: {Value}", expressionString, value);
 		else
-			Write(level, "{Expression}: {Value}, {Message}", expressionString, value, message);
+			logger.Write(level, "{Expression}: {Value}, {Message}", expressionString, value, message);
 	}
 }
